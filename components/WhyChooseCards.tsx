671d2e1faf9a15f20c0c1f4569b1e4f5941b3b0f
EmirIsKing <emirstormzy2@gmail.com>
2025-06-27 17:27:28 +0000

    More type error fixes

--- components/WhyChooseCards.tsx
+++ components/WhyChooseCards.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import LightningSvg from '@/public/LightningSvg'
 
-const WhyChooseCards = ({title, details1, details2, Icon}) => {
+const WhyChooseCards = ({title, details1, details2, Icon}:{title:string, details1: string, details2:string, Icon:any}) => {
   return (
     <div className='p-7 bg-white/5 rounded-md border border-gray-50/40 drop-shadow-md flex flex-col justify-center items-center gap-3'>
         <Icon/>
